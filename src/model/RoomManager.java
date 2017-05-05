230301f50e8558febc6468d600a8459108c2882d
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2017-05-04 20:48:59 -0700

    Room Manager can now look up rooms based on their ID.

--- src/model/RoomManager.java
+++ src/model/RoomManager.java
@@ -1,6 +1,10 @@
 package model;
 
 import java.io.Serializable;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 /**
  * A room manager that holds a graph of nodes.
@@ -8,13 +12,17 @@ import java.io.Serializable;
  * Responsibilities:
  *      keep track of the starting room.
  *      generate unique room IDs.
+ *      look up rooms from their ID
  * 
  * @author Zachary Chandler
  */
-public class RoomManager implements Serializable {
+public final class RoomManager implements Serializable {
 
     /** Generated SVUID */
     private static final long serialVersionUID = -8083744640096022029L;
+    
+    /** A map of roomID's to rooms for fast lookup. */
+    private Map<Integer, RoomNode> rooms;
     
     /** The initial room in the graph, any room not connected to this one isn't part of the graph. */
     private RoomNode startingRoom;
@@ -37,6 +45,9 @@ public class RoomManager implements Serializable {
         
         this.startingRoom = startingRoom;
         this.nextRoomID = startingRoom.getRoomID() + 1;
+        this.rooms = new TreeMap<>();
+        
+        this.purgeDisconnectedRooms();
     }
     
     /**
@@ -68,5 +79,52 @@ public class RoomManager implements Serializable {
         }
         
         this.startingRoom = startingRoom;
+    }
+    
+    /**
+     * Get the room for this ID. If this room hasn't been added, it will not be found.
+     * @param roomID the ID to search for.
+     * @return the RoomNode or null if it is not found.
+     */
+    public RoomNode getRoom(int roomID) {
+        return this.rooms.get(roomID);
+    }
+    
+    /**
+     * Adds the given room to the tracked nodes.
+     * @param room the room to add.
+     */
+    public void trackRoom(RoomNode room) {
+        this.rooms.put(room.getRoomID(), room);
+    }
+    
+    /**
+     * Searches and adds all rooms connected to the starting node that are not being tracked.
+     */
+    public void addAllConnectedRooms() {
+        addAllConnectedRooms(new TreeSet<RoomNode>(), this.startingRoom);
+    }
+    
+    /**
+     * Removes all known rooms that are disconnected from the starting room.
+     */
+    public void purgeDisconnectedRooms() {
+        this.rooms = new TreeMap<>();
+        addAllConnectedRooms();
+    }
+    
+    
+    private void addAllConnectedRooms(Set<RoomNode> seenRooms, RoomNode room) {
+        if (room != null && !seenRooms.contains(room)) {
+            seenRooms.add(room);
+            this.trackRoom(room);
+            
+            addAllConnectedRooms(seenRooms, room.getUp());
+            addAllConnectedRooms(seenRooms, room.getDown());
+            addAllConnectedRooms(seenRooms, room.getNorth());
+            addAllConnectedRooms(seenRooms, room.getWest());
+            addAllConnectedRooms(seenRooms, room.getEast());
+            addAllConnectedRooms(seenRooms, room.getSouth());
+        }
     }
 }
